7a2c476854c738893e9078af47f2582eef3ccb9d
macservis <fboqiyev1065@gmail.com>
2023-03-13 15:24:11 +0500

    stage0-module4-conditions-task4 updated

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        if ((third > second && second > first) || (third > second && second < first)){
+            System.out.println(third);
+        } else if ((second > third && third >= first) || (second > third && third <= first)) {
+            System.out.println(second);
+        }else {
+            System.out.println(first);
+        }
     }
 }
